61f1d14604ae4752d5136440cb7ffae5dfdcbe64
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-10-06 01:42:35 -0400

    added dropdown vertical offset for the menu

--- app/src/main/java/com/adefemikolawole/ggcstudentpicker/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/ggcstudentpicker/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getSimpleName();
@@ -32,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.st_picker_menu ,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==R.id.menu_about){
+            Toast.makeText(MainActivity.this, "Author: Adefemi Kolawole\nITEC 4500 -- Fall 2018", Toast.LENGTH_LONG).show();
+
+        }
+        return true;
+
     }
 
     public void changeStatusActionBarColor(){
