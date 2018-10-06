d8bbc76a2b184669711ef94694b3bd287ea4bfea
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-10-05 23:41:14 -0400

    changed color of status bar and aciton bar to green as per specs.

--- app/src/main/java/com/adefemikolawole/ggcstudentpicker/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/ggcstudentpicker/MainActivity.java
@@ -1,12 +1,17 @@
 package com.adefemikolawole.ggcstudentpicker;
-
+import android.R.menu;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    public static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        changeStatusActionBarColor();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.st_picker_menu, menu);
+        inflater.inflate(R.menu.st_picker_menu ,menu);
         return true;
+    }
+
+    public void changeStatusActionBarColor(){
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.green)));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.darkGreen));
+        }
     }
 }
